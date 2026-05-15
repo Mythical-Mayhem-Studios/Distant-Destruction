311b2ba036b3cb037d4cc2b3f9295b7f506dcf8d
Waffle <csshin9928@gmail.com>
2026-05-16 00:34:39 +0900

    feat: create shield events

---
+++ src/shared/networking/ShieldEvents.ts
@@ -0,0 +1,13 @@
+import { Networking } from "@flamework/networking";
+
+interface ClientToServerEvents {}
+
+interface ServerToClientEvents {
+	maxShieldChangedEvent(maxShield: number): void;
+	shieldChangedEvent(shield: number): void;
+}
+
+export const GlobalShieldEvents = Networking.createEvent<ClientToServerEvents, ServerToClientEvents>();
+
+export const ClientShieldEvents = GlobalShieldEvents.createClient({});
+export const ServerShieldEvents = GlobalShieldEvents.createServer({});
