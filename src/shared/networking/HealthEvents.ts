4fee96b4f2e64e33fabff6c2e9f28dcf0805cc87
Waffle <csshin9928@gmail.com>
2026-05-16 00:32:59 +0900

    feat: create health events

---
+++ src/shared/networking/HealthEvents.ts
@@ -0,0 +1,13 @@
+import { Networking } from "@flamework/networking";
+
+interface ClientToServerEvents {}
+
+interface ServerToClientEvents {
+	maxHealthChangedEvent(maxHealth: number): void;
+	healthChangedEvent(health: number): void;
+}
+
+export const GlobalHealthEvents = Networking.createEvent<ClientToServerEvents, ServerToClientEvents>();
+
+export const ClientHealthEvents = GlobalHealthEvents.createClient({});
+export const ServerHealthEvents = GlobalHealthEvents.createServer({});
